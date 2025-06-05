d1801c2c330a6a82b713d66b8212c6d87ad55747
Pacifica <shelkaev86@gmail.com>
2025-06-05 17:16:07 +0300

    env fix try

--- server/models/index.js
+++ server/models/index.js
@@ -1,84 +1,34 @@
 const { Sequelize, DataTypes } = require('sequelize');
 require('dotenv').config();
 
-let sequelize
-
-if (process.env.DATABASE_URL) {
-  sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialect: 'postgres',
-    logging: false,
-    dialectOptions: {
-      ssl:{
-        equire: true,
-        rejectUnauthorized: false,
-      },
-    },
-  });
-}else{
-  sequelize=new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: 'postgres',
-    logging: false
-    }
-  );
-}
-
-
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  logging: false,
+  dialectOptions:
+    process.env.DB_SSL === 'true'
+      ? { ssl: { require: true, rejectUnauthorized: false } }
+      : {},
+});
 
 const User = sequelize.define('User', {
-  username: {
-    type: DataTypes.STRING,
-    unique: true,
-    allowNull: false
-  },
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false
-  }
+  username: { type: DataTypes.STRING, unique: true, allowNull: false },
+  password: { type: DataTypes.STRING,                 allowNull: false },
 });
 
 const Voting = sequelize.define('Voting', {
-  title: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
+  title:       { type: DataTypes.STRING,  allowNull: false },
   description: DataTypes.TEXT,
-  options: {
-    type: DataTypes.ARRAY(DataTypes.STRING),
-    allowNull: false,
-    defaultValue: []
-  },
-  results: {
-    type: DataTypes.JSONB,
-    allowNull: false,
-    defaultValue: {}
-  },
-  completed: {
-    type: DataTypes.BOOLEAN,
-    defaultValue: false
-  },
-  votedUsers: {
-    type: DataTypes.ARRAY(DataTypes.INTEGER),
-    defaultValue: []
-  }
+  options:     { type: DataTypes.ARRAY(DataTypes.STRING), allowNull: false, defaultValue: [] },
+  results:     { type: DataTypes.JSONB,  allowNull: false, defaultValue: {} },
+  completed:   { type: DataTypes.BOOLEAN, defaultValue: false },
+  votedUsers:  { type: DataTypes.ARRAY(DataTypes.INTEGER), defaultValue: [] },
 });
 
 User.hasMany(Voting, { foreignKey: 'UserId' });
 Voting.belongsTo(User, { foreignKey: 'UserId' });
 
-sequelize
-  .authenticate()
-  .then(() => console.log('✅ база данных подключена'))
-  .catch(err => console.error('❌ нет доступа к базе данных:', err));
+sequelize.authenticate()
+  .then(() => console.log('✅ База данных подключена'))
+  .catch(err  => console.error('❌ Ошибка подключения к БД:', err));
 
-
-module.exports = {
-  db: sequelize,
-  User,
-  Voting
-};
+module.exports = { db: sequelize, User, Voting };
