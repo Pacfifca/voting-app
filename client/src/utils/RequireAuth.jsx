74c2cb8ba3df12e994cba3be2fc5f7a773a69101
Pacfifca <85892684+Pacfifca@users.noreply.github.com>
2025-06-05 22:07:47 +0300

    Update RequireAuth.jsx

--- client/src/utils/RequireAuth.jsx
+++ client/src/utils/RequireAuth.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
 
 const RequireAuth = () => {
-  /* токен всё ещё в localStorage ? */
   const hasToken = Boolean(localStorage.getItem('token'));
 
   return hasToken ? <Outlet /> : <Navigate to="/auth" replace />;
